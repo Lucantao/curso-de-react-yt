8fe81bb8666b04119f2f8252a19047574b37f3e9
Jonathan Brambati <brambati.moreira@gmail.com>
2026-06-28 17:15:02 -0300

    correção da tela de tarefa

--- src/Pages/TaskPage.jsx
+++ src/Pages/TaskPage.jsx
@@ -10,7 +10,7 @@ function TaskPage() {
 
   return (
     <div className="h-screen w-screen bg-slate-500 p-6">
-      <div className="w- [500px] space-y-4">
+      <div className="w- [500px] mx-auto space-y-4">
         <div className="flex justify-center relative mb-6">
           <button 
           onClick={() => navigate(-1)}
@@ -21,7 +21,7 @@ function TaskPage() {
         <Title>
           Detalhes da Tarefa
         </Title>
-        
+
         </div>
            <div className="bg-slate-200 p-4 rounded-md">
             <h2 className="text-xl font-bold text-slate-600">{title}</h2>
